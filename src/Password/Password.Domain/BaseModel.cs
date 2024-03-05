ad3858370ed3e37e0438168fb95adde245c5f42f
Rasmus Kristensen <rtk@umbraco.dk>
2024-03-05 12:09:27 +0100

    Created domain model

--- src/Password/Password.Domain/BaseModel.cs
+++ src/Password/Password.Domain/BaseModel.cs
@@ -2,8 +2,9 @@
 public class BaseModel
 {
     public Guid Id { get; private protected init; }
-    public DateTime CreatedUtc { get; protected init; }
+    public DateTime CreatedUtc { get; protected init; } 
     public DateTime ModifiedUtc { get; private protected init; }
+    public bool Deleted { get; protected init; }
 
     protected BaseModel(Guid id)
     {
@@ -15,10 +16,11 @@ public class BaseModel
 
     }
 
-    public BaseModel(Guid id, DateTime createdUtc, DateTime modifiedUtc)
+    protected BaseModel(Guid id, DateTime createdUtc, DateTime modifiedUtc, bool deleted)
     {
         Id = id;
         CreatedUtc = createdUtc;
         ModifiedUtc = modifiedUtc;
+        Deleted = deleted;
     }
 }
