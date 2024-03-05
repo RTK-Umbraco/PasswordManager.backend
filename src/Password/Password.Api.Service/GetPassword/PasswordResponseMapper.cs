53f2355b9cabc07ac223513390b5af5fc4f1054f
Rasmus Kristensen <rtk@umbraco.dk>
2024-03-05 13:23:29 +0100

    #29 Created endpoint to get password

---
+++ src/Password/Password.Api.Service/GetPassword/PasswordResponseMapper.cs
@@ -0,0 +1,17 @@
+﻿using PasswordManager.Password.Domain.Password;
+
+namespace PasswordManager.Password.Api.Service.GetPassword;
+
+public static class PasswordResponseMapper
+{
+    public static PasswordResponse Map(PasswordModel passwordModel)
+    {
+        var passwordResponse = new PasswordResponse(passwordModel.Id,
+                                                    passwordModel.Url,
+                                                    passwordModel.FriendlyName,
+                                                    passwordModel.Username,
+                                                    passwordModel.Password);
+
+        return passwordResponse;
+    }
+}
